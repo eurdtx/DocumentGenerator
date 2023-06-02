5027e7483ab9bf661a138951b0e3ad9b18eccd8c
eurdtx <135347111+eurdtx@users.noreply.github.com>
2023-06-02 11:53:13 -0500

    Uploaded run executable command file

---
+++ GenerateLabResultReport/CommandFiles/run.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+REM The below line will need to be filled in with the appropriate file paths and folder paths
+[Path to GenerateLabResultReport.exe] [Path to text file with list of sample IDs] [Path to output folder of PDfs] >> [Path to Log Text File]
+
+REM Comment out the below line if you wish to delete the input file after processing
+del [Path to text file with list of sample IDs]
